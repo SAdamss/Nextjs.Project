0462798aec59aa02e2b9a08f1e42edebe80251ab
Shannon <shannonadams083@gmail.com>
2022-05-27 21:19:25 +0100

    config

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -90,14 +90,14 @@ const Skills = () => {
         <div className='grid grid-cols-2 gap-4 justify-center items-center'>
           <div className='m-auto'>
             <Image
-              src='/../public/assets/skills/github.png'
+              src='/../public/assets/skills/react.png'
               width='64px'
               height='64px'
               alt='/'
             />
           </div>
           <div className='flex flex-col items-center justify-center'>
-            <h3>GitHub</h3>
+            <h3>React</h3>
           </div>
         </div>
       </div>

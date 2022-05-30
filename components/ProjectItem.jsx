628fe2b0235fa71a3a6b434248eb4d91fab2629c
Shannon <shannonadams083@gmail.com>
2022-05-30 20:55:42 +0100

    portfolio update

--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -13,6 +13,7 @@ export const ProjectItem = ({title, backgroundImg, projectUrl}) => {
             <p className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>More Info</p>
         </Link>
         <Image className='rounded-xl group-hover:opacity-10' src={backgroundImg} alt='/' /> 
+        </div>
     <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
         <h3 className='text-2xl text-white tracking-wider text-center'>{title}</h3>
         <p className='pb-4 pt-2 text-white text-center'>React JS</p>
@@ -23,7 +24,6 @@ export const ProjectItem = ({title, backgroundImg, projectUrl}) => {
     
 
     </div>
- </div>
  
   )
 }

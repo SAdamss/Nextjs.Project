c04f394eaa3f39252a629268ff631d48d601c4b1
Shannon <shannonadams083@gmail.com>
2022-05-30 21:01:36 +0100

    new project1

--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -9,21 +9,13 @@ export const ProjectItem = ({title, backgroundImg, projectUrl}) => {
     <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
         <h3 className='text-2xl text-white tracking-wider text-center'>{title}</h3>
         <p className='pb-4 pt-2 text-white text-center'>React JS</p>
-        <Link href="https://sophiedouglasperformer.com" alt={projectUrl}>
-            <p className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>More Info</p>
-        </Link>
-        <Image className='rounded-xl group-hover:opacity-10' src={backgroundImg} alt='/' /> 
-        </div>
-    <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
-        <h3 className='text-2xl text-white tracking-wider text-center'>{title}</h3>
-        <p className='pb-4 pt-2 text-white text-center'>React JS</p>
-        <Link href="https://github.com/SAdamss/travel-app-react" alt={projectUrl}>
+        <Link href={projectUrl}>
             <p className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>More Info</p>
         </Link>
     </div>
     
-
-    </div>
+   
+ </div>
  
   )
 }
